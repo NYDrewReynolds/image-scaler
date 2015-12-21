f13511690ab53ff1674d43eb60c12d63252a4942
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-12-21 15:57:33 -0500

    handle multiple pairs

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,18 +4,21 @@ class Image < ActiveRecord::Base
     set_bounding_box(bounding_box)
     scaled_dimensions = []
     @ratio = 0
-    num_of_pairs = (image_dimensions.length / 2)
-    width = image_dimensions[0]
-    height = image_dimensions[1]
+    # num_of_pairs = (image_dimensions.length / 2)  Use later to check even number is sent
 
-    if width >= height
-      @ratio = @bounded_width.to_f/width
-    else
-      @ratio = @bounded_height.to_f/height
-    end
+    image_dimensions.each_slice(2) { |pair|
+      width  = pair[0]
+      height = pair[1]
 
-    scaled_dimensions << (width * @ratio).to_i
-    scaled_dimensions << (height * @ratio).to_i
+      if width >= height
+        @ratio = @bounded_width.to_f/width
+      else
+        @ratio = @bounded_height.to_f/height
+      end
+
+      scaled_dimensions << (width * @ratio).to_i
+      scaled_dimensions << (height * @ratio).to_i
+    }
 
     {scaled_dimensions: scaled_dimensions,
      bounding_box: bounding_box}
